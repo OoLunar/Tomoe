9c538b8198ce9c9edd017265d8bfcf35654dc589
Lunar <lunar@forsaken-borders.net>
2021-01-01 19:21:46 -0600

    Fixed muted role, added antimeme role.

--- src/commands/Moderation/Config.cs
+++ src/commands/Moderation/Config.cs
@@ -10,13 +10,13 @@ using Tomoe.Utils;
 namespace Tomoe.Commands.Config
 {
 	[Group("config")]
-	public class Mute : BaseCommandModule
+	public class Config : BaseCommandModule
 	{
 		private static readonly Logger MuteLogger = new("Commands.Config.Mute");
 		private static readonly Logger NoMemeLogger = new("Commands.Config.NoMeme");
 
 		[Command("mute"), Description("Sets up or assigns the mute role."), RequireUserPermissions(Permissions.ManageGuild), RequireGuild]
-		public async Task SetupMute(CommandContext context, DiscordRole muteRole)
+		public async Task Mute(CommandContext context, DiscordRole muteRole)
 		{
 			ulong? previousMuteRoleId = Program.Database.Guild.MuteRole(context.Guild.Id);
 			if (previousMuteRoleId.HasValue && previousMuteRoleId.Value != muteRole.Id)
@@ -45,7 +45,7 @@ namespace Tomoe.Commands.Config
 							_ = Program.SendMessage(context, $"Roles were left untouched.");
 						}
 					});
-					DiscordMessage discordMessage = Program.SendMessage(context, $"Previous mute role was <{previousMuteRoleId.Value}>. Do you want to overwrite it with {muteRole.Mention}?");
+					DiscordMessage discordMessage = Program.SendMessage(context, $"Previous mute role was <@&{previousMuteRoleId.Value}>. Do you want to overwrite it with {muteRole.Mention}?");
 					await discordMessage.CreateReactionAsync(thumbsUp);
 					await discordMessage.CreateReactionAsync(thumbsDown);
 					createNewRole.MessageId = discordMessage.Id;
@@ -60,7 +60,7 @@ namespace Tomoe.Commands.Config
 		}
 
 		[Command("mute"), RequireUserPermissions(Permissions.ManageGuild), RequireBotPermissions(Permissions.ManageRoles), RequireGuild]
-		public async Task SetupMute(CommandContext context)
+		public async Task Mute(CommandContext context)
 		{
 			ulong? previousMuteRoleId = Program.Database.Guild.MuteRole(context.Guild.Id);
 			if (previousMuteRoleId.HasValue)
@@ -135,131 +135,122 @@ namespace Tomoe.Commands.Config
 			}
 		}
 
-		[Command("nomeme"), Description("Sets up or assigns the no meme role."), RequireUserPermissions(Permissions.ManageGuild), RequireGuild, Aliases(new[] { "anti_meme", "antimeme", "memeban", "meme_ban", "no_meme" })]
-		public async Task SetupNoMeme(CommandContext context, DiscordRole noMemeRole)
+		[Command("antimeme"), Description("Sets up or assigns the antimeme role."), RequireUserPermissions(Permissions.ManageGuild), RequireGuild, Aliases(new[] { "anti_meme", "nomeme", "no_meme", "memeban", "meme_ban" })]
+		public async Task AntiMeme(CommandContext context, DiscordRole antiMemeRole)
 		{
-			ulong? previousNoMemeRoleId = Program.Database.Guild.NoMemeRole(context.Guild.Id);
-			DiscordRole previousNoMemeRole;
-			if (previousNoMemeRoleId.HasValue && previousNoMemeRoleId.Value != noMemeRole.Id)
+			ulong? previousAntiMemeRoleId = Program.Database.Guild.NoMemeRole(context.Guild.Id);
+			if (previousAntiMemeRoleId.HasValue && previousAntiMemeRoleId.Value != antiMemeRole.Id)
 			{
-				previousNoMemeRole = context.Guild.GetRole(previousNoMemeRoleId.Value);
-				if (previousNoMemeRole != null) goto Prompt;
-				else goto SetNoMemeRole;
-			}
-			else goto SetNoMemeRole;
-
-			Prompt:
-			{
-				DiscordEmoji thumbsUp = DiscordEmoji.FromUnicode(context.Client, "👍");
-				DiscordEmoji thumbsDown = DiscordEmoji.FromUnicode(context.Client, "👎");
-				ReactionAdded.Queue createNewRole = new();
-				createNewRole.User = context.User;
-				createNewRole.Emojis = new DiscordEmoji[] { thumbsUp, thumbsDown };
-				createNewRole.Action = new ReactionAdded.ReactionHandler(async emoji =>
+				DiscordRole previousAntiMemeRole = context.Guild.GetRole(previousAntiMemeRoleId.Value);
+				if (previousAntiMemeRole != null)
 				{
-					if (emoji == thumbsUp)
+					DiscordEmoji thumbsUp = DiscordEmoji.FromUnicode(context.Client, "👍");
+					DiscordEmoji thumbsDown = DiscordEmoji.FromUnicode(context.Client, "👎");
+					ReactionAdded.Queue createNewRole = new();
+					createNewRole.User = context.User;
+					createNewRole.Emojis = new DiscordEmoji[] { thumbsUp, thumbsDown };
+					createNewRole.Action = new ReactionAdded.ReactionHandler(async emoji =>
 					{
-						Program.Database.Guild.NoMemeRole(context.Guild.Id, noMemeRole.Id);
-						NoMemeLogger.Trace($"Set {noMemeRole.Name} ({noMemeRole.Id}) as the no meme role for {context.Guild.Name} ({context.Guild.Id})!");
-						await FixNoMemeRolePermissions(context.Guild, noMemeRole);
-						_ = Program.SendMessage(context, $"{noMemeRole.Mention} is now set as the no meme role.");
-					}
-					else if (emoji == thumbsDown)
-					{
-						NoMemeLogger.Trace($"Set previous no meme role {previousNoMemeRole.Name} ({previousNoMemeRole.Id}) as the no meme role for {context.Guild.Name} ({context.Guild.Id})!");
-						await FixNoMemeRolePermissions(context.Guild, previousNoMemeRole);
-						_ = Program.SendMessage(context, $"{previousNoMemeRole.Mention} is now set as the no meme role.");
-					}
-				});
-				DiscordMessage discordMessage = Program.SendMessage(context, $"Previous no meme role was <{previousNoMemeRoleId.Value}>. Do you want to overwrite it with {noMemeRole.Mention}?");
-				await discordMessage.CreateReactionAsync(thumbsUp);
-				await discordMessage.CreateReactionAsync(thumbsDown);
-				createNewRole.MessageId = discordMessage.Id;
-				ReactionAdded.QueueList.Add(createNewRole);
-			};
-
-		SetNoMemeRole:
-			{
-				Program.Database.Guild.NoMemeRole(context.Guild.Id, noMemeRole.Id);
-				DiscordMessage discordMessage = Program.SendMessage(context, $"Setting role permissions for {noMemeRole.Mention}...");
-				await FixNoMemeRolePermissions(context.Guild, noMemeRole);
-				_ = discordMessage.ModifyAsync($"{context.User.Mention}: {noMemeRole.Mention} is now set as the no meme role.", null, new List<IMention>() { new UserMention(context.User.Id) });
-			}
-		}
-
-		[Command("nomeme"), RequireUserPermissions(Permissions.ManageGuild), RequireBotPermissions(Permissions.ManageRoles), RequireGuild]
-		public async Task SetupNoMeme(CommandContext context)
-		{
-			ulong? previousNoMemeRoleId = Program.Database.Guild.NoMemeRole(context.Guild.Id);
-			if (previousNoMemeRoleId.HasValue)
-			{
-				DiscordRole previousNoMemeRole = context.Guild.GetRole(previousNoMemeRoleId.Value);
-				if (previousNoMemeRole == null)
-				{
-					await CreateNoMemeRole(context);
+						if (emoji == thumbsUp)
+						{
+							Program.Database.Guild.NoMemeRole(context.Guild.Id, antiMemeRole.Id);
+							NoMemeLogger.Trace($"Set {antiMemeRole.Name} ({antiMemeRole.Id}) as the antimeme role for {context.Guild.Name} ({context.Guild.Id})!");
+							await FixAntiMemeRolePermissions(context.Guild, antiMemeRole);
+							_ = Program.SendMessage(context, $"{antiMemeRole.Mention} is now set as the antimeme role.");
+						}
+						else if (emoji == thumbsDown)
+						{
+							NoMemeLogger.Trace($"Fixed permissions for the antimeme role {previousAntiMemeRole.Name} ({previousAntiMemeRole.Id}) on {context.Guild.Name} ({context.Guild.Id})!");
+							await FixAntiMemeRolePermissions(context.Guild, previousAntiMemeRole);
+							_ = Program.SendMessage(context, $"Roles were left untouched.");
+						}
+					});
+					DiscordMessage discordMessage = Program.SendMessage(context, $"Previous antimeme role was {previousAntiMemeRole.Mention}. Do you want to overwrite it with {antiMemeRole.Mention}?");
+					await discordMessage.CreateReactionAsync(thumbsUp);
+					await discordMessage.CreateReactionAsync(thumbsDown);
+					createNewRole.MessageId = discordMessage.Id;
+					ReactionAdded.QueueList.Add(createNewRole);
 					return;
 				}
-				DiscordEmoji thumbsUp = DiscordEmoji.FromUnicode(context.Client, "👍");
-				DiscordEmoji thumbsDown = DiscordEmoji.FromUnicode(context.Client, "👎");
-				ReactionAdded.Queue createNewRole = new();
-				createNewRole.User = context.User;
-				createNewRole.Emojis = new DiscordEmoji[] { thumbsUp, thumbsDown };
-				createNewRole.Action = new ReactionAdded.ReactionHandler(async emoji =>
-				{
-					if (emoji == thumbsUp)
-					{
-						await CreateNoMemeRole(context);
-					}
-					else if (emoji == thumbsDown)
-					{
-						_ = Program.SendMessage(context, $"Roles were left untouched.");
-					}
-				});
-				DiscordMessage discordMessage = Program.SendMessage(context, $"Previous no meme role was {previousNoMemeRole.Mention}. Do you want to overwrite it?");
-				createNewRole.MessageId = discordMessage.Id;
-				await discordMessage.CreateReactionAsync(thumbsUp);
-				await discordMessage.CreateReactionAsync(thumbsDown);
-				ReactionAdded.QueueList.Add(createNewRole);
-				return;
 			}
-			else
-			{
-				await CreateNoMemeRole(context);
-			}
+			Program.Database.Guild.NoMemeRole(context.Guild.Id, antiMemeRole.Id);
+			NoMemeLogger.Trace($"Set {antiMemeRole.Name} ({antiMemeRole.Id}) as the antimeme role for {context.Guild.Name} ({context.Guild.Id})!");
+			await FixAntiMemeRolePermissions(context.Guild, antiMemeRole);
+			_ = Program.SendMessage(context, $"{antiMemeRole.Mention} is now set as the antimeme role.");
 		}
 
-		public static async Task CreateNoMemeRole(CommandContext context)
+		[Command("antimeme"), RequireUserPermissions(Permissions.ManageGuild), RequireBotPermissions(Permissions.ManageRoles), RequireGuild]
+		public async Task AntiMeme(CommandContext context)
 		{
-			DiscordMessage message = Program.SendMessage(context, "Creating no meme role...");
-			DiscordRole muteRole = await context.Guild.CreateRoleAsync("No Meme", Permissions.None, DiscordColor.Gray, false, false, "Allows users to be no memed.");
-			Program.Database.Guild.NoMemeRole(context.Guild.Id, muteRole.Id);
-			NoMemeLogger.Trace($"Created no meme role '{muteRole.Name}' ({muteRole.Id}) for {context.Guild.Name} ({context.Guild.Id})!");
-			_ = message.ModifyAsync($"{context.User.Mention}: Overriding channel permissions...", null, new List<IMention>() { new UserMention(context.User.Id) });
-			await FixNoMemeRolePermissions(context.Guild, muteRole);
-			_ = await message.ModifyAsync($"{context.User.Mention}: Done! No meme role is now {muteRole.Mention}", null, new List<IMention>() { new UserMention(context.User.Id) });
+			ulong? previousAntiMemeRoleId = Program.Database.Guild.NoMemeRole(context.Guild.Id);
+			if (previousAntiMemeRoleId.HasValue)
+			{
+				DiscordRole previousAntiMemeRole = context.Guild.GetRole(previousAntiMemeRoleId.Value);
+				if (previousAntiMemeRole != null)
+				{
+					DiscordEmoji thumbsUp = DiscordEmoji.FromUnicode(context.Client, "👍");
+					DiscordEmoji thumbsDown = DiscordEmoji.FromUnicode(context.Client, "👎");
+					ReactionAdded.Queue createNewRole = new();
+					createNewRole.User = context.User;
+					createNewRole.Emojis = new DiscordEmoji[] { thumbsUp, thumbsDown };
+					createNewRole.Action = new ReactionAdded.ReactionHandler(async emoji =>
+					{
+						if (emoji == thumbsUp)
+						{
+							await CreateAntiMemeRole(context);
+						}
+						else if (emoji == thumbsDown)
+						{
+							NoMemeLogger.Trace($"Fixed permissions for the antimeme role {previousAntiMemeRole.Name} ({previousAntiMemeRole.Id}) on {context.Guild.Name} ({context.Guild.Id})!");
+							await FixAntiMemeRolePermissions(context.Guild, previousAntiMemeRole);
+							_ = Program.SendMessage(context, $"Roles were left untouched.");
+						}
+					});
+					DiscordMessage discordMessage = Program.SendMessage(context, $"Previous antimeme role was {previousAntiMemeRole.Mention}. Do you want to overwrite it?");
+					createNewRole.MessageId = discordMessage.Id;
+					await discordMessage.CreateReactionAsync(thumbsUp);
+					await discordMessage.CreateReactionAsync(thumbsDown);
+					ReactionAdded.QueueList.Add(createNewRole);
+					return;
+				}
+			}
+			// Should only be executed if there was no previous mute role id, or if the role cannot be found.
+			await CreateAntiMemeRole(context);
 		}
 
-		public static async Task FixNoMemeRolePermissions(DiscordGuild guild, DiscordRole muteRole)
+		public static async Task CreateAntiMemeRole(CommandContext context)
+		{
+			DiscordMessage message = Program.SendMessage(context, "Creating antimeme role...");
+			DiscordRole muteRole = await context.Guild.CreateRoleAsync("Antimeme", Permissions.None, DiscordColor.Gray, false, false, "Allows users to be no memed.");
+			NoMemeLogger.Trace($"Created antimeme role \"{muteRole.Name}\" ({muteRole.Id}) for {context.Guild.Name} ({context.Guild.Id})!");
+			Program.Database.Guild.NoMemeRole(context.Guild.Id, muteRole.Id);
+			_ = message.ModifyAsync($"{context.User.Mention}: Overriding channel permissions...", null, new List<IMention>() { new UserMention(context.User.Id) });
+			await FixAntiMemeRolePermissions(context.Guild, muteRole);
+			_ = await message.ModifyAsync($"{context.User.Mention}: Done! Antimeme role is now {muteRole.Mention}", null, new List<IMention>() { new UserMention(context.User.Id) });
+		}
+
+		public static async Task FixAntiMemeRolePermissions(DiscordGuild guild, DiscordRole muteRole)
 		{
 			foreach (DiscordChannel channel in guild.Channels.Values)
 			{
 				switch (channel.Type)
 				{
 					case ChannelType.Text:
-						NoMemeLogger.Trace($"Overwriting permission {Permissions.SendMessages} and {Permissions.AddReactions} for no meme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
-						_ = channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.AttachFiles | Permissions.AddReactions | Permissions.EmbedLinks | Permissions.UseExternalEmojis, "Stops members with the role from spamming chat with reactions, images or links.").ConfigureAwait(false).GetAwaiter();
+						NoMemeLogger.Trace($"Overwriting permission {Permissions.SendMessages} and {Permissions.AddReactions} for antimeme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
+						await channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.AttachFiles | Permissions.AddReactions | Permissions.EmbedLinks | Permissions.UseExternalEmojis, "Stops members with the role from spamming chat with reactions, images or links.");
 						await Task.Delay(50);
 						break;
 					case ChannelType.Voice:
-						NoMemeLogger.Trace($"Overwriting permission {Permissions.Speak} and {Permissions.Stream} for no meme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
-						_ = channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.Stream | Permissions.UseVoiceDetection, "Disallows users to spam starting and stopping streams. Must push to talk.");
+						NoMemeLogger.Trace($"Overwriting permission {Permissions.Speak} and {Permissions.Stream} for antimeme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
+						await channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.Stream | Permissions.UseVoiceDetection, "Disallows users to spam starting and stopping streams. Must push to talk.");
 						await Task.Delay(50);
 						break;
 					case ChannelType.Category:
-						NoMemeLogger.Trace($"Overwriting permission {Permissions.SendMessages}, {Permissions.AddReactions}, {Permissions.Speak} and {Permissions.Stream} for no meme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
-						_ = channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.AttachFiles | Permissions.AddReactions | Permissions.EmbedLinks | Permissions.UseExternalEmojis | Permissions.Stream | Permissions.UseVoiceDetection, "Stops members with the role from spamming chat with reactions, images or links/voice channels and through streams. Disallows users to spam starting and stopping streams. Must push to talk.");
+						NoMemeLogger.Trace($"Overwriting permission {Permissions.SendMessages}, {Permissions.AddReactions}, {Permissions.Speak} and {Permissions.Stream} for antimeme role {muteRole.Name} ({muteRole.Id}) on {channel.Type} channel {channel.Name} ({channel.Id}) for {guild.Name} ({guild.Id})...");
+						await channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.AttachFiles | Permissions.AddReactions | Permissions.EmbedLinks | Permissions.UseExternalEmojis | Permissions.Stream | Permissions.UseVoiceDetection, "Stops members with the role from spamming chat with reactions, images or links/voice channels and through streams. Disallows users to spam starting and stopping streams. Must push to talk.");
 						await Task.Delay(50);
 						break;
+					default: break;
 				}
 			}
 		}
