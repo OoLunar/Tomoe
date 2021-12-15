0854fb4ad865abd23496b6919f8139241ee4fb3c
Lunar Starstrum <lunar@forsaken-borders.net>
2021-12-15 12:02:28 -0600

    Update logging for better debugging in prod

--- src/Commands/Listeners/CommandErrored.cs
+++ src/Commands/Listeners/CommandErrored.cs
@@ -5,12 +5,17 @@ namespace Tomoe.Commands
     using DSharpPlus.SlashCommands;
     using DSharpPlus.SlashCommands.EventArgs;
     using Humanizer;
+    using Serilog;
     using System;
+    using System.Globalization;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
 
     public partial class Listeners
     {
+        private static readonly ILogger Logger = Log.ForContext<Listeners>();
+
         public static async Task CommandErrored(SlashCommandsExtension slashCommandExtension, SlashCommandErrorEventArgs slashCommandErrorEventArgs)
         {
             if (slashCommandErrorEventArgs.Exception is SlashExecutionChecksFailedException)
@@ -24,8 +29,17 @@ namespace Tomoe.Commands
             DiscordChannel discordChannel = await slashCommandExtension.Client.GetChannelAsync(832374606748188743);
             DiscordMessageBuilder discordMessageBuilder = new();
             string stackTrace = string.Join("\n\n", slashCommandErrorEventArgs.Exception.StackTrace.Split('\n').Select(line => line.Trim())).Truncate(1800);
-            discordMessageBuilder.Content = $"`/{slashCommandErrorEventArgs.Context.CommandName}` threw a {slashCommandErrorEventArgs.Exception.GetType()}: {slashCommandErrorEventArgs.Exception.Message ?? "<no message>"}\n{Formatter.BlockCode(stackTrace, "cs")}";
+            StringBuilder stringBuilder = new();
+            stringBuilder.AppendLine(CultureInfo.InvariantCulture, $"`/{slashCommandErrorEventArgs.Context.CommandName}` threw a {slashCommandErrorEventArgs.Exception.GetType()}: {slashCommandErrorEventArgs.Exception.Message ?? "<no message>"}");
+            if (slashCommandErrorEventArgs.Exception.InnerException != null)
+            {
+                stringBuilder.AppendLine(CultureInfo.InvariantCulture, $"Inner Exception: {slashCommandErrorEventArgs.Exception.InnerException.GetType()}: {slashCommandErrorEventArgs.Exception.InnerException.Message ?? "<no message>"}");
+            }
+            stringBuilder.AppendLine(Formatter.BlockCode(stackTrace, "cs"));
+            discordMessageBuilder.Content = stringBuilder.ToString();
             await discordChannel.SendMessageAsync(discordMessageBuilder);
+
+            Logger.Error("{exception}", slashCommandErrorEventArgs.Exception);
         }
     }
 }
