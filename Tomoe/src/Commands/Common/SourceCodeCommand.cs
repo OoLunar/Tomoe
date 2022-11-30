cf853f9cb7473a462f8817c101fd231f0d4c3b30
Lunar Starstrum <lunar@forsaken-borders.net>
2022-11-30 01:28:01 -0600

    Add source code and support commands

---
+++ Tomoe/src/Commands/Common/SourceCodeCommand.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using DSharpPlus;
+using OoLunar.DSharpPlus.CommandAll.Attributes;
+using OoLunar.DSharpPlus.CommandAll.Commands;
+
+namespace OoLunar.Tomoe.Commands.Common
+{
+    public sealed class SourceCodeCommand : BaseCommand
+    {
+        private static readonly string _sourceCodeUrl = typeof(Program).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>().First(attribute => attribute.Key == "RepositoryUrl").Value!;
+
+        [Command("source_code")]
+        public static Task ExecuteAsync(CommandContext context) => context.ReplyAsync(new() { Content = $"You can find my source code here: {Formatter.EmbedlessUrl(new(_sourceCodeUrl))}" });
+    }
+}
---
+++ Tomoe/src/Commands/Common/SupportCommand.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using OoLunar.DSharpPlus.CommandAll.Attributes;
+using OoLunar.DSharpPlus.CommandAll.Commands;
+
+namespace OoLunar.Tomoe.Commands.Common
+{
+    public sealed class SupportCommand : BaseCommand
+    {
+        private readonly string? _supportServerInvite;
+
+        public SupportCommand(IConfiguration configuration) => _supportServerInvite = configuration.GetValue<string>("discord:debug_guild_id");
+
+        [Command("support")]
+        public Task ExecuteAsync(CommandContext context) => context.ReplyAsync(new()
+        {
+            Content = _supportServerInvite is null
+                ? "I'm sorry, but the owner of the bot doesn't seem to have setup a support server."
+                : $"Are you looking for support? You can join my support server here: <{_supportServerInvite}>"
+        });
+    }
+}
