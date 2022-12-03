b4095931cf42163ad278d97febc93941ec31e693
Lunar Starstrum <lunar@forsaken-borders.net>
2022-12-02 22:30:32 -0600

    I've seen my sins and I repent them. Please forgive me CA Gods

--- Tomoe/src/Commands/Common/AvatarCommand.cs
+++ Tomoe/src/Commands/Common/AvatarCommand.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using DSharpPlus;
@@ -23,108 +21,44 @@ namespace OoLunar.Tomoe.Commands.Common
 
         public AvatarCommand(HttpClient httpClient) => HttpClient = httpClient;
 
-        public override async Task BeforeExecutionAsync(CommandContext context)
+        [Command("user")]
+        public Task UserAsync(CommandContext context, DiscordUser? user = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0)
+            => SendAvatarAsync(context, $"{(user ??= context.User).Username}{(user.Username.EndsWith('s') ? "'" : "'s")} Avatar", user.GetAvatarUrl(imageFormat, imageDimensions), user.BannerColor.HasValue && !user.BannerColor.Value.Equals(default(DiscordColor)) ? user.BannerColor.Value : null);
+
+        [Command("webhook"), SuppressMessage("Roslyn", "IDE0046", Justification = "Avoid the ternary operator rabbit hole")]
+        public Task WebhookAsync(CommandContext context, DiscordMessage? message = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0)
         {
-            ushort imageDimensions = (ushort)context.NamedArguments.Last().Value!;
-            if (imageDimensions == 0)
+            if (context.IsSlashCommand || message is not null)
             {
-                imageDimensions = 1024;
-            }
-            else if (imageDimensions < 16 || imageDimensions > 4096 || (imageDimensions & (imageDimensions - 1)) != 0)
-            {
-                await context.ReplyAsync("The image dimensions must be `16`, `32`, `64`, `128`, `256`, `512`, `1024`, `2048` or `4096`");
-                return;
-            }
-
-            ImageFormat imageFormat = (ImageFormat)context.NamedArguments.Skip(1).First().Value!;
-            if (imageFormat is ImageFormat.Unknown)
-            {
-                imageFormat = ImageFormat.Png;
-            }
-
-            DiscordEmbedBuilder embedBuilder = new() { Color = new DiscordColor("#6b73db") };
-            string url;
-            KeyValuePair<CommandParameter, object> firstArgument = context.NamedArguments.First()!;
-            Type parameterType = Nullable.GetUnderlyingType(firstArgument.Key.ParameterInfo.ParameterType) ?? firstArgument.Key.ParameterInfo.ParameterType;
-            if (parameterType == typeof(DiscordUser))
-            {
-                DiscordUser user = firstArgument.Value as DiscordUser ?? context.User;
-                embedBuilder.Title = $"{user.Username}{(user.Username.EndsWith('s') ? "'" : "'s")} Avatar";
-
-                // If the user was obtained from an API request, attempt to grab their banner color.
-                if (user.BannerColor.HasValue && !user.BannerColor.Value.Equals(default(DiscordColor)))
-                {
-                    embedBuilder.Color = user.BannerColor.Value;
-                }
-
-                url = user.GetAvatarUrl(imageFormat, imageDimensions);
-            }
-            else if (parameterType == typeof(DiscordMember))
-            {
-                if (context.Guild is null)
-                {
-                    await context.ReplyAsync($"`/{context.CurrentCommand.FullName}` can only be used in a guild!");
-                    return;
-                }
-                else
-                {
-                    DiscordMember member = firstArgument.Value as DiscordMember ?? context.Member!;
-                    embedBuilder.Title = $"{member.Username}{(member.Username.EndsWith('s') ? "'" : "'s")} Guild Avatar";
-                    if (!member.Color.Equals(default(DiscordColor)))
-                    {
-                        embedBuilder.Color = member.Color;
-                    }
-
-                    url = member.GetGuildAvatarUrl(imageFormat, imageDimensions);
-                }
-            }
-            else if (parameterType == typeof(DiscordMessage))
-            {
-                if (context.IsSlashCommand || firstArgument.Value is not null)
-                {
-                    DiscordMessage message = (DiscordMessage)firstArgument.Value;
-                    embedBuilder.Title = $"{message.Author.Username}{(message.Author.Username.EndsWith('s') ? "'" : "'s")} Avatar";
-                    url = message.Author.GetAvatarUrl(imageFormat, imageDimensions);
-                }
-                else
-                {
-                    if (context.Message!.ReferencedMessage is null)
-                    {
-                        await context.ReplyAsync("Please reply to a message or provide a message link of whose avatar to grab.");
-                        return;
-                    }
-                    else
-                    {
-                        embedBuilder.Title = $"{context.Message.ReferencedMessage.Author.Username}{(context.Message.ReferencedMessage.Author.Username.EndsWith('s') ? "'" : "'s")} Avatar";
-                        url = context.Message.ReferencedMessage.Author.GetAvatarUrl(imageFormat, imageDimensions);
-                    }
-                }
+                return SendAvatarAsync(context, $"{message!.Author.Username}{(message.Author.Username.EndsWith('s') ? "'" : "'s")} Avatar", message.Author.GetAvatarUrl(imageFormat, imageDimensions));
             }
             else
             {
-                throw new NotImplementedException(context.CurrentCommand.FullName);
+                return context.Message!.ReferencedMessage is null
+                    ? context.ReplyAsync("Please reply to a message or provide a message link of whose avatar to grab.")
+                    : SendAvatarAsync(context, $"{context.Message.ReferencedMessage.Author.Username}{(context.Message.ReferencedMessage.Author.Username.EndsWith('s') ? "'" : "'s")} Avatar", context.Message.ReferencedMessage.Author.GetAvatarUrl(imageFormat, imageDimensions));
             }
-
-            await SendAvatarAsync(context, embedBuilder, url);
         }
 
-        [Command("user")]
-        public static Task UserAsync(CommandContext context, DiscordUser? user = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0) => Task.CompletedTask;
-
-        [Command("webhook")]
-        public static Task WebhookAsync(CommandContext context, DiscordMessage? message = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0) => Task.CompletedTask;
-
         [Command("guild")]
-        public static Task GuildAsync(CommandContext context, DiscordMember? member = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0) => Task.CompletedTask;
+        public Task GuildAsync(CommandContext context, DiscordMember? member = null, [ArgumentConverter<EnumArgumentConverter>] ImageFormat imageFormat = ImageFormat.Auto, ushort imageDimensions = 0) => context.Guild is null
+            ? context.ReplyAsync($"`/{context.CurrentCommand.FullName}` can only be used in a guild!")
+            : SendAvatarAsync(context, $"{(member ??= context.Member!).Username}{(member.Username.EndsWith('s') ? "'" : "'s")} Guild Avatar", member.GetGuildAvatarUrl(imageFormat, imageDimensions), !member.Color.Equals(default(DiscordColor)) ? member.Color : null);
 
-        private async Task SendAvatarAsync(CommandContext context, DiscordEmbedBuilder embedBuilder, string url)
+        private async Task SendAvatarAsync(CommandContext context, string embedTitle, string url, DiscordColor? embedColor = null)
         {
             await context.DelayAsync();
             Stream imageStream = await (await HttpClient.GetAsync(url)).Content.ReadAsStreamAsync();
             Image image = await Image.LoadAsync(imageStream);
             imageStream.Position = 0; // For image format in the next field.
 
-            embedBuilder.ImageUrl = url;
+            DiscordEmbedBuilder embedBuilder = new()
+            {
+                Title = embedTitle,
+                ImageUrl = url,
+                Color = embedColor ?? new DiscordColor("#6b73db"),
+            };
+
             embedBuilder.AddField("Image Format", Image.DetectFormat(imageStream).Name, true);
             embedBuilder.AddField("Frame Count", image.Frames.Count.ToString("N0"), true);
             embedBuilder.AddField("File Size", imageStream.Length.Bytes().Humanize(), true);
