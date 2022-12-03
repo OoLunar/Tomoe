dc965a4f18bf7346a6c948141e61191dfe069dc1
Lunar Starstrum <lunar@forsaken-borders.net>
2022-12-03 14:40:37 -0600

    Have `>>bot info` use the database to count things

--- Tomoe/src/Commands/Common/BotInfoCommand.cs
+++ Tomoe/src/Commands/Common/BotInfoCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
@@ -12,15 +13,19 @@ using OoLunar.DSharpPlus.CommandAll.Attributes;
 using OoLunar.DSharpPlus.CommandAll.Commands;
 using OoLunar.DSharpPlus.CommandAll.Managers;
 using OoLunar.DSharpPlus.CommandAll.Parsers;
+using OoLunar.Tomoe.Database;
 
 namespace OoLunar.Tomoe.Commands.Common
 {
     public sealed partial class BotInfoCommand : BaseCommand
     {
         private static readonly ReadOnlyMemory<char> _slashPrefix = new[] { ',', ' ', '`', '/', '`' };
+        private readonly DatabaseContext _databaseContext;
+
+        public BotInfoCommand(DatabaseContext databaseContext) => _databaseContext = databaseContext ?? throw new ArgumentNullException(nameof(databaseContext));
 
         [Command("bot_info")]
-        public static Task ExecuteAsync(CommandContext context)
+        public Task ExecuteAsync(CommandContext context)
         {
             DiscordEmbedBuilder embedBuilder = new()
             {
@@ -30,16 +35,16 @@ namespace OoLunar.Tomoe.Commands.Common
 
             Process currentProcess = Process.GetCurrentProcess();
             currentProcess.Refresh();
-            embedBuilder.AddField("Heap Memory", GC.GetTotalMemory(false).Bytes().ToString("MB", CultureInfo.InvariantCulture), true);
-            embedBuilder.AddField("Process Memory", currentProcess.WorkingSet64.Bytes().ToString("MB", CultureInfo.InvariantCulture), true);
-            embedBuilder.AddField("Total Memory Available", currentProcess.PrivateMemorySize64.Bytes().ToString("MB", CultureInfo.InvariantCulture), true);
-
+            embedBuilder.AddField("Heap Memory", GC.GetTotalMemory(false).Bytes().ToString(), true);
+            embedBuilder.AddField("Process Memory", currentProcess.WorkingSet64.Bytes().ToString(), true);
             embedBuilder.AddField("Runtime Version", RuntimeInformation.FrameworkDescription, true);
+
             embedBuilder.AddField("Operating System", $"{Environment.OSVersion} {RuntimeInformation.OSArchitecture.ToString().ToLower(CultureInfo.InvariantCulture)}", true);
             embedBuilder.AddField("Uptime", LastCommaRegex().Replace((Process.GetCurrentProcess().StartTime - DateTime.Now).Humanize(3), " and "), true);
+            embedBuilder.AddField("Websocket Ping", LastCommaRegex().Replace(context.Client.Ping.Milliseconds().Humanize(3), " and "), true);
 
-            embedBuilder.AddField("Guild Count", context.Client.Guilds.Count.ToMetric(), true);
-            embedBuilder.AddField("Websocket Ping", context.Client.Ping + "ms", true);
+            embedBuilder.AddField("Guild Count", _databaseContext.Guilds.LongCount().ToString("N0"), true);
+            embedBuilder.AddField("User Count", _databaseContext.Members.LongCount().ToString("N0"), true);
 
             /*
             List<string> prefixes = new();
