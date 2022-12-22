02ba7ebbacfab19a34042228260ef46777762373
Lunar Starstrum <lunar@forsaken-borders.net>
2022-12-22 16:44:18 -0600

    New timestamp command

---
+++ Tomoe/src/Commands/Common/TimestampCommand.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using OoLunar.DSharpPlus.CommandAll.Attributes;
+using OoLunar.DSharpPlus.CommandAll.Commands;
+
+namespace OoLunar.Tomoe.Commands.Common
+{
+    public sealed class TimestampCommand : BaseCommand
+    {
+        [Command("timestamp")]
+        public static Task ExecuteAsync(CommandContext context, TimestampFormat format = TimestampFormat.LongDateTime, TimeSpan? when = null) => ExecuteAsync(context, format, when.HasValue ? DateTime.UtcNow.Add(when.Value) : DateTime.UtcNow);
+
+        [Command("timestamp")]
+        public static Task ExecuteAsync(CommandContext context, TimestampFormat format = TimestampFormat.LongDateTime, DateTime? when = null) => context.ReplyAsync($"Timestamp: {Formatter.Timestamp(when ?? DateTime.UtcNow, format)}");
+    }
+}
