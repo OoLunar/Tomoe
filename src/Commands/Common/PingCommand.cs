24f791cec44b5bf08325354f22d19a2a7bffe426
Lunar Starstrum <lunar@forsaken-borders.net>
2024-04-04 18:13:03 -0500

    Permissions requirements

--- src/Commands/Common/PingCommand.cs
+++ src/Commands/Common/PingCommand.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.Commands;
+using DSharpPlus.Commands.ContextChecks;
 using DSharpPlus.Commands.Processors.TextCommands.Attributes;
 using DSharpPlus.Commands.Trees;
 using DSharpPlus.Commands.Trees.Attributes;
@@ -8,7 +10,7 @@ namespace OoLunar.Tomoe.Commands.Common
 {
     public static class PingCommand
     {
-        [Command("ping"), TextAlias("pong")]
+        [Command("ping"), TextAlias("pong"), RequirePermissions(Permissions.SendMessages | Permissions.SendMessagesInThreads | Permissions.AccessChannels)]
         public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync($"Pong! Latency is {context.Client.Ping}ms.");
     }
 }
