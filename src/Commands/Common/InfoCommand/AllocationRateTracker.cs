8438d99123151801f69a354d0afeae5d09f49e17
Lunar Starstrum <lunar@forsaken-borders.net>
2024-05-06 00:10:01 -0500

    More accurately track the memory usage

--- src/Commands/Common/InfoCommand/AllocationRateTracker.cs
+++ src/Commands/Common/InfoCommand/AllocationRateTracker.cs
@@ -4,18 +4,27 @@ using System.Threading.Tasks;
 
 namespace OoLunar.Tomoe
 {
+    /// <summary>
+    /// Tracks the memory allocation rate of the application.
+    /// </summary>
     public sealed class AllocationRateTracker
     {
+        /// <summary>
+        /// The current allocation rate of the application's total process memory.
+        /// </summary>
         public long AllocationRate { get; private set; } = GC.GetTotalAllocatedBytes();
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="AllocationRateTracker"/>, starting the allocation rate tracking in the background.
+        /// </summary>
+        /// <returns></returns>
         public AllocationRateTracker() => _ = TrackAllocationRateAsync();
 
         private async Task TrackAllocationRateAsync()
         {
-            long previousMemoryUsage = GC.GetTotalAllocatedBytes();
-
             // We use a PeriodicTimer to track and update the allocation rate every 200ms
             PeriodicTimer periodicTimer = new(TimeSpan.FromSeconds(1));
+            long previousMemoryUsage = GC.GetTotalAllocatedBytes();
             while (await periodicTimer.WaitForNextTickAsync())
             {
                 long currentMemoryUsage = GC.GetTotalAllocatedBytes();
