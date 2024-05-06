ad98d46ad937511c8be06a77de2bad978d803c59
Lunar Starstrum <lunar@forsaken-borders.net>
2024-05-05 20:24:58 -0500

    Improve upon user command

--- src/Commands/Common/InfoCommand/InfoCommand.User.cs
+++ src/Commands/Common/InfoCommand/InfoCommand.User.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Commands;
+using DSharpPlus.Commands.Trees.Metadata;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
+using Humanizer;
 using OoLunar.Tomoe.Database.Models;
 
 namespace OoLunar.Tomoe.Commands.Common
@@ -14,10 +18,12 @@ namespace OoLunar.Tomoe.Commands.Common
         /// Sends information about the provided user.
         /// </summary>
         /// <param name="user">Which user to get information about. Leave empty to get information about yourself.</param>
-        [Command("user")]
+        [Command("user"), TextAlias("member")]
         public static async Task UserInfoAsync(CommandContext context, DiscordUser? user = null)
         {
             user ??= context.User;
+
+            GuildMemberModel? memberModel = await GuildMemberModel.FindMemberAsync(context.User.Id, context.Guild!.Id);
             DiscordEmbedBuilder embedBuilder = new()
             {
                 Title = $"Info about {user.GetDisplayName()}",
@@ -25,36 +31,98 @@ namespace OoLunar.Tomoe.Commands.Common
                 Color = new DiscordColor("#6b73db")
             };
 
-            embedBuilder.AddField("Mention", user.Mention, true);
-            if (user is DiscordMember member)
+            embedBuilder.AddField("User Id", Formatter.InlineCode(user.Id.ToString(CultureInfo.InvariantCulture)), true);
+            embedBuilder.AddField("User Mention", user.Mention, true);
+
+            List<string> userFlags = [];
+            if (!user.Flags.HasValue)
             {
-                if (!member.Color.Equals(default(DiscordColor)))
-                {
-                    embedBuilder.Color = member.Color;
-                }
-
-                GuildMemberModel? memberModel = await GuildMemberModel.FindMemberAsync(context.User.Id, context.Guild!.Id);
-                if (memberModel is not null && memberModel.FirstJoined != member.JoinedAt.UtcDateTime)
-                {
-                    embedBuilder.AddField("User Id", Formatter.InlineCode(user.Id.ToString(CultureInfo.InvariantCulture)), false);
-                    embedBuilder.AddField("Joined Discord", Formatter.Timestamp(user.CreationTimestamp, TimestampFormat.RelativeTime), true);
-                    embedBuilder.AddField("First joined the Server", Formatter.Timestamp(memberModel.FirstJoined, TimestampFormat.RelativeTime), true);
-                }
-                else
-                {
-                    embedBuilder.AddField("User Id", Formatter.InlineCode(user.Id.ToString(CultureInfo.InvariantCulture)), true);
-                    // ZWS field
-                    embedBuilder.AddField("\u200B", "\u200B", true);
-                    embedBuilder.AddField("Joined Discord", Formatter.Timestamp(user.CreationTimestamp, TimestampFormat.RelativeTime), true);
-                }
-
-                embedBuilder.AddField("Recently joined the Server", Formatter.Timestamp(member.JoinedAt, TimestampFormat.RelativeTime), true);
-                embedBuilder.AddField("Roles", member.Roles.Any() ? string.Join('\n', member.Roles.OrderByDescending(role => role.Position).Select(role => $"- {role.Mention}")) : "None", false);
+                userFlags.Add("None");
             }
             else
             {
-                embedBuilder.AddField("User Id", Formatter.InlineCode(user.Id.ToString(CultureInfo.InvariantCulture)), true);
-                embedBuilder.AddField("Joined Discord", Formatter.Timestamp(user.CreationTimestamp, TimestampFormat.RelativeTime), true);
+                for (int i = 0; i < (sizeof(DiscordUserFlags) * 8); i++)
+                {
+                    DiscordUserFlags flag = (DiscordUserFlags)(1 << i);
+                    if (!user.Flags.Value.HasFlag(flag))
+                    {
+                        continue;
+                    }
+
+                    // If the flag isn't documented, Humanize will return an empty string.
+                    // When that happens, we'll use the flag bit instead.
+                    string displayFlag = flag.Humanize().ToLowerInvariant();
+                    if (string.IsNullOrWhiteSpace(displayFlag))
+                    {
+                        // For whatever reason, the spammer flag is intentionally
+                        // undocumented as "bots will never have a use for it".
+                        displayFlag = i == 20 ? "Likely spammer" : $"1 << {i}";
+                    }
+
+                    // Capitalize the first letter of the first flag.
+                    if (userFlags.Count == 0)
+                    {
+                        displayFlag = char.ToUpper(displayFlag[0], CultureInfo.InvariantCulture) + displayFlag[1..];
+                    }
+
+                    userFlags.Add(displayFlag);
+                }
+            }
+
+            embedBuilder.AddField("User Flags", userFlags.DefaultIfEmpty($"Unknown flags: {user.Flags}").Humanize(), true);
+            embedBuilder.AddField("Joined Discord", Formatter.Timestamp(user.CreationTimestamp, TimestampFormat.RelativeTime), true);
+
+            // This means they're not in the server and they've never joined previously.
+            if (memberModel is null)
+            {
+                await context.RespondAsync(embedBuilder);
+                return;
+            }
+
+            // The user probably wasn't in the cache. Let's try to get them from the guild.
+            if (user is not DiscordMember member)
+            {
+                try
+                {
+                    member = await context.Guild!.GetMemberAsync(user.Id);
+                }
+                // The user is not in the guild.
+                catch (DiscordException)
+                {
+                    embedBuilder.AddField("First Joined the Server", Formatter.Timestamp(memberModel.FirstJoined, TimestampFormat.RelativeTime), true);
+                    embedBuilder.AddField("User Record", memberModel.State.Humanize(), true);
+                    await context.RespondAsync(embedBuilder);
+                    return;
+                }
+            }
+
+            // Check to see if the first join date is older than the current join date.
+            if (memberModel.FirstJoined > member.JoinedAt)
+            {
+                memberModel = memberModel with
+                {
+                    FirstJoined = member.JoinedAt.UtcDateTime
+                };
+
+                await memberModel.UpdateAsync();
+            }
+
+            // The user is in the guild.
+            embedBuilder.AddField("First Joined the Server", Formatter.Timestamp(memberModel.FirstJoined, TimestampFormat.RelativeTime), true);
+
+            // This will happen when the user has joined and left the server multiple times.
+            if (memberModel.FirstJoined != member.JoinedAt)
+            {
+                embedBuilder.AddField("Recently Joined the Server", Formatter.Timestamp(member.JoinedAt, TimestampFormat.RelativeTime), true);
+            }
+
+            embedBuilder.AddField("User Record", memberModel.State.Humanize(), true);
+            embedBuilder.AddField("Roles", member.Roles.Any() ? string.Join('\n', member.Roles.OrderByDescending(role => role.Position).Select(role => $"- {role.Mention}")) : "None", false);
+
+            // If the user has a color, set it.
+            if (!member.Color.Equals(default(DiscordColor)))
+            {
+                embedBuilder.Color = member.Color;
             }
 
             await context.RespondAsync(embedBuilder);
--- src/Database/Models/GuildMemberState.cs
+++ src/Database/Models/GuildMemberState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 
 namespace OoLunar.Tomoe.Database.Models
 {
@@ -8,21 +9,25 @@ namespace OoLunar.Tomoe.Database.Models
         /// <summary>
         /// The member is present and has no special flags.
         /// </summary>
+        [Description("Nothing of note")]
         None = 0,
 
         /// <summary>
         /// The member has previously joined the guild however is not currently present.
         /// </summary>
+        [Description("Not in the guild")]
         Absent = 1 << 0,
 
         /// <summary>
         /// The member is currently muted.
         /// </summary>
+        [Description("Currently muted")]
         Muted = 1 << 1,
 
         /// <summary>
         /// The member is currently banned.
         /// </summary>
+        [Description("Currently banned")]
         Banned = 1 << 2,
     }
 }
