dde51396c6fe697b2ca0e07286b467d7853f1436
Lunar Starstrum <lunar@forsaken-borders.net>
2024-10-01 11:33:10 -0500

    Update MoveCommand.cs

--- src/Commands/Moderation/MoveCommand.cs
+++ src/Commands/Moderation/MoveCommand.cs
@@ -74,9 +74,8 @@ namespace OoLunar.Tomoe.Commands.Moderation
             foreach (DiscordMessage message in messages.OrderBy(x => x.CreationTimestamp))
             {
                 webhookBuilder = new DiscordWebhookBuilder(new DiscordMessageBuilder(message));
-                DiscordMember member = (DiscordMember)message.Author!;
-                webhookBuilder.WithUsername(member.DisplayName);
-                webhookBuilder.WithAvatarUrl(member.GuildAvatarUrl ?? member.AvatarUrl);
+                webhookBuilder.WithUsername(message.Author!.GetDisplayName());
+                webhookBuilder.WithAvatarUrl(message.Author is DiscordMember member ? member.GuildAvatarUrl : message.Author!.AvatarUrl);
                 if (channel.Type is DiscordChannelType.NewsThread or DiscordChannelType.PublicThread or DiscordChannelType.PrivateThread)
                 {
                     webhookBuilder.WithThreadId(channel.Id);
