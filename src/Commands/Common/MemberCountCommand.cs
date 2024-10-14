e2987a76547af78ec7bf3a7235a60a68a0123af9
Lunar Starstrum <lunar@forsaken-borders.net>
2024-10-14 12:02:15 -0500

    Add `mc` alias to `member_count`

--- src/Commands/Common/MemberCountCommand.cs
+++ src/Commands/Common/MemberCountCommand.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using DSharpPlus.Commands;
 using DSharpPlus.Commands.ContextChecks;
 using DSharpPlus.Commands.Trees;
+using DSharpPlus.Commands.Trees.Metadata;
 using OoLunar.Tomoe.Database.Models;
 
 namespace OoLunar.Tomoe.Commands.Common
@@ -14,7 +15,7 @@ namespace OoLunar.Tomoe.Commands.Common
         /// <summary>
         /// Sends the number of members in the guild.
         /// </summary>
-        [Command("member_count"), RequireGuild]
+        [Command("member_count"), RequireGuild, TextAlias("mc")]
         public static async ValueTask ExecuteAsync(CommandContext context) => await context.RespondAsync($"Current member count: {await GuildMemberModel.CountMembersAsync(context.Guild!.Id):N0}");
     }
 }
