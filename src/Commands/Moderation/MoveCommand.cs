77b61865820c4fb116d610950b68895f1567851a
Lunar Starstrum <lunar@forsaken-borders.net>
2024-10-28 14:01:26 -0500

    Change move command to forward command

--- src/Commands/Moderation/MoveCommand.cs
+++ src/Commands/Moderation/MoveCommand.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus.Commands;
 using DSharpPlus.Commands.ContextChecks;
@@ -14,29 +16,145 @@ namespace OoLunar.Tomoe.Commands.Moderation
     /// <summary>
     /// Moved my heart.
     /// </summary>
-    public sealed class MoveCommand
+    public sealed class ForwardCommand
     {
-        private readonly HttpClient httpClient;
+        private static readonly TimeSpan _tenSeconds = TimeSpan.FromSeconds(10);
 
         /// <summary>
-        /// Creates a new instance of <see cref="MoveCommand"/>.
+        /// We're using a ULID's timestamp to store when the last message was sent (to detect ratelimits)
+        /// and the randomness to store the completion percentage.
         /// </summary>
-        /// <param name="httpClient">Required service for retrieving remote files.</param>
-        public MoveCommand(HttpClient httpClient) => this.httpClient = httpClient;
+        private Ulid _completionPercent;
 
         /// <summary>
         /// Copies a chunk of messages to a different channel.
         /// </summary>
         /// <param name="channel">Which channel to send the messages to.</param>
-        /// <param name="firstMessage">Where to start copying messages from.</param>
+        /// <param name="firstMessage">Where to start copying messages from. This message will be moved too.</param>
         /// <param name="lastMessage">Where to stop copying messages from. If not provided, will copy all messages after the first message.</param>
         [Command("move"), Description("Moves a chunk of messages (inclusive) to a different channel."), RequirePermissions(DiscordPermissions.ManageMessages | DiscordPermissions.ReadMessageHistory)]
         public async ValueTask MoveAsync(CommandContext context, DiscordChannel channel, DiscordMessage firstMessage, DiscordMessage? lastMessage = null)
         {
-            await context.DeferResponseAsync();
+            await context.RespondAsync("Moving messages...");
+
+            CancellationTokenSource cancellationTokenSource = new();
+            Task idleTask = IdleAsync(context, channel, cancellationTokenSource.Token);
+
+            // Create a webhook builder to reuse for all messages
+            DiscordWebhookBuilder webhookBuilder = new()
+            {
+                Username = $"{context.Guild!.CurrentMember.Username} (Message Mover)",
+                AvatarUrl = context.Guild.CurrentMember.AvatarUrl,
+                Content = $"I've started moving messages. This may take a moment.\nFirst message: {firstMessage.JumpLink}\nLast message: {(lastMessage is null ? "Latest message." : lastMessage.JumpLink)}"
+            };
+
+            // Create the webhook
+            DiscordWebhook webhook;
+            if (channel.Type is DiscordChannelType.NewsThread or DiscordChannelType.PublicThread or DiscordChannelType.PrivateThread)
+            {
+                webhook = await channel.Parent.CreateWebhookAsync("Message Mover", Optional.FromNoValue<Stream>(), "Requested by " + context.User.Username);
+                webhookBuilder.WithThreadId(channel.Id);
+            }
+            else
+            {
+                webhook = await channel.CreateWebhookAsync("Message Mover", Optional.FromNoValue<Stream>(), "Requested by " + context.User.Username);
+            }
+
+            await webhook.ExecuteAsync(webhookBuilder);
 
             List<DiscordUser> users = [];
             List<DiscordMessage> messages = [];
+            await GatherMessagesAsync(messages, users, firstMessage, lastMessage);
+            await ForwardMessagesAsync(webhook, channel, messages);
+
+            await webhook.ExecuteAsync(new DiscordWebhookBuilder()
+            {
+                Username = $"{context.Guild.CurrentMember.Username} (Message Mover)",
+                AvatarUrl = context.Guild.CurrentMember.AvatarUrl,
+                Content = $"I've finished moving messages. {messages.Count:N0} messages have been moved.\n-# Cc {string.Join(", ", users.OrderBy(user => user.Id).Select(x => x.Mention))}"
+            });
+
+            await webhook.DeleteAsync();
+            await cancellationTokenSource.CancelAsync();
+            await idleTask;
+
+            if (context.Channel.LastMessageId == (await context.GetResponseAsync())?.Id)
+            {
+                // If there haven't been any other messages between the last message and the response, edit the response.
+                await context.EditResponseAsync($"{messages.Count:N0} messages have been moved.");
+            }
+            else
+            {
+                // If there have been other messages between the last message and the response, send a new message.
+                await context.RespondAsync($"{messages.Count:N0} messages have been moved.");
+            }
+        }
+
+        private async Task IdleAsync(CommandContext context, DiscordChannel channel, CancellationToken cancellationToken = default)
+        {
+            // Lock the channel to prevent other messages from being sent
+            List<DiscordOverwriteBuilder> overwrites = [];
+            foreach (DiscordOverwrite overwrite in channel.PermissionOverwrites)
+            {
+                overwrites.Add(await DiscordOverwriteBuilder.FromAsync(overwrite));
+            }
+
+            List<DiscordOverwriteBuilder> newOverwrites = [];
+            foreach (DiscordOverwriteBuilder overwrite in overwrites)
+            {
+                // Shallow copy
+                DiscordOverwriteBuilder newOverwrite = overwrite with { };
+
+                // If we're moving messages into threads, prevent users from sending messages in threads
+                newOverwrite.Deny(channel.IsThread
+                    ? DiscordPermissions.SendMessagesInThreads | DiscordPermissions.ManageMessages
+                    : DiscordPermissions.SendMessages | DiscordPermissions.ManageMessages | DiscordPermissions.CreatePublicThreads | DiscordPermissions.CreatePrivateThreads);
+
+                newOverwrites.Add(newOverwrite);
+            }
+
+            // Add an overwrite that'll allow the bot to send messages
+            newOverwrites.Add(new DiscordOverwriteBuilder()
+            {
+                Target = channel.Guild.CurrentMember,
+                Type = DiscordOverwriteType.Member,
+                Allowed = channel.IsThread ? DiscordPermissions.SendMessagesInThreads : DiscordPermissions.SendMessages
+            });
+
+            // Remove send messages, manage messages (pins), and create
+            // threads permissions so the message history doesn't get fucked.
+            await channel.ModifyAsync(channelEditModel => channelEditModel.PermissionOverwrites = newOverwrites);
+
+            // Periodically send a typing indicator to let others know the bot is still working.
+            PeriodicTimer timer = new(_tenSeconds);
+            cancellationToken.Register(timer.Dispose);
+
+            Ulid lastCompletionPercent = _completionPercent;
+            while (await timer.WaitForNextTickAsync(default))
+            {
+                await channel.TriggerTypingAsync();
+                if (_completionPercent != lastCompletionPercent)
+                {
+                    StringBuilder stringBuilder = new($"Moving messages... {_completionPercent.Random[0]}% done...");
+
+                    // If it's been more than 10 seconds since the time has last updated, we've hit a ratelimit
+                    if ((DateTimeOffset.UtcNow - _completionPercent.Time) > _tenSeconds)
+                    {
+                        stringBuilder.Append(" Warning! Discord's ratelimits have been hit! Per Discord's request, the bot will pause for a few minutes before continuing.");
+                    }
+
+                    await context.EditResponseAsync(stringBuilder.ToString());
+                    lastCompletionPercent = _completionPercent;
+                }
+            }
+
+            // Restore the previous permission overwrites
+            await channel.ModifyAsync(channelEditModel => channelEditModel.PermissionOverwrites = overwrites);
+        }
+
+        private static async ValueTask GatherMessagesAsync(List<DiscordMessage> messages, List<DiscordUser> users, DiscordMessage firstMessage, DiscordMessage? lastMessage = null)
+        {
+            // Gather all messages to move
             await foreach (DiscordMessage message in firstMessage.Channel!.GetMessagesAfterAsync(firstMessage.Id))
             {
                 if (!users.Contains(message.Author!))
@@ -51,70 +169,33 @@ namespace OoLunar.Tomoe.Commands.Moderation
                 }
             }
 
-            DiscordWebhookBuilder webhookBuilder = new()
-            {
-                Username = $"{context.Guild!.CurrentMember.Username} (Message Mover)",
-                AvatarUrl = context.Guild.CurrentMember.AvatarUrl,
-                Content = $"I've started moving messages. This may take a moment.\nFirst message: {firstMessage.JumpLink}\nLast message: {(lastMessage is null ? "Latest message." : lastMessage.JumpLink)}"
-            };
+            // Sort the messages by when they were sent
+            messages.Sort((x, y) => x.Id.CompareTo(y.Id));
+        }
 
-            DiscordWebhook webhook;
+        private async ValueTask ForwardMessagesAsync(DiscordWebhook webhook, DiscordChannel channel, IReadOnlyList<DiscordMessage> messages)
+        {
+            DiscordWebhookBuilder webhookBuilder = new();
             if (channel.Type is DiscordChannelType.NewsThread or DiscordChannelType.PublicThread or DiscordChannelType.PrivateThread)
             {
-                webhook = await channel.Parent.CreateWebhookAsync("Message Mover", Optional.FromNoValue<Stream>(), "Requested by " + context.User.Username);
                 webhookBuilder.WithThreadId(channel.Id);
             }
-            else
+
+            for (int i = 0; i < messages.Count; i++)
             {
-                webhook = await channel.CreateWebhookAsync("Message Mover", Optional.FromNoValue<Stream>(), "Requested by " + context.User.Username);
+                DiscordMessage message = messages[i];
+                if (i == 0 || message.Author != messages[i - 1].Author)
+                {
+                    webhookBuilder.Username = message.Author!.GetDisplayName();
+                    webhookBuilder.AvatarUrl = message.Author is DiscordMember member && !string.IsNullOrWhiteSpace(member.GuildAvatarUrl) ? member.GuildAvatarUrl : message.Author!.AvatarUrl;
+                    webhookBuilder.Content = $"{message.Author.Mention} said:";
+                    await webhook.ExecuteAsync(webhookBuilder);
+                }
+
+                // Forward the message
+                await message.ForwardAsync(channel);
+                _completionPercent = Ulid.NewUlid(DateTimeOffset.UtcNow, [(byte)Math.Clamp((int)(i / (double)messages.Count * 100), 0, 100), 0, 0, 0, 0, 0, 0, 0, 0, 0]);
             }
-
-            await webhook.ExecuteAsync(webhookBuilder);
-            foreach (DiscordMessage message in messages.OrderBy(x => x.Id))
-            {
-                webhookBuilder = new DiscordWebhookBuilder(new DiscordMessageBuilder(message));
-                webhookBuilder.WithUsername(message.Author!.GetDisplayName());
-                webhookBuilder.WithAvatarUrl(message.Author is DiscordMember member ? member.GuildAvatarUrl : message.Author!.AvatarUrl);
-                if (channel.Type is DiscordChannelType.NewsThread or DiscordChannelType.PublicThread or DiscordChannelType.PrivateThread)
-                {
-                    webhookBuilder.WithThreadId(channel.Id);
-                }
-
-                if (!string.IsNullOrWhiteSpace(message.ReferencedMessage?.Content) && message.ReferencedMessage.Content.Length + webhookBuilder.Content?.Length + 3 < 2000)
-                {
-                    webhookBuilder.WithContent($"> {message.ReferencedMessage.Content}\n{webhookBuilder.Content}");
-                }
-
-                if (message.Attachments.Count != 0)
-                {
-                    List<string> attachments = [];
-                    for (int i = 0; i < message.Attachments.Count; i++)
-                    {
-                        DiscordAttachment attachment = message.Attachments[i];
-                        if (attachments.Contains(attachment.FileName!))
-                        {
-                            webhookBuilder.AddFile(attachment.FileName + i.ToString(await context.GetCultureAsync()), await httpClient.GetStreamAsync(attachment.Url), false);
-                        }
-                        else
-                        {
-                            webhookBuilder.AddFile(attachment.FileName!, await httpClient.GetStreamAsync(attachment.Url), false);
-                        }
-
-                        attachments.Add(attachment.FileName!);
-                    }
-                }
-
-                if (message.Components?.Count is not null and not 0)
-                {
-                    webhookBuilder.AddComponents(message.Components);
-                }
-
-                await webhook.ExecuteAsync(webhookBuilder);
-            }
-
-            await webhook.ExecuteAsync(new DiscordWebhookBuilder().WithUsername(context.Guild.CurrentMember.Username + " (Message Mover)").WithAvatarUrl(context.Guild.CurrentMember.AvatarUrl).WithContent($"I've finished moving messages. {messages.Count:N0} messages have been moved.\n-# Cc {string.Join(", ", users.OrderBy(user => user.Id).Select(x => x.Mention))}"));
-            await webhook.DeleteAsync();
-            await context.RespondAsync($"{messages.Count:N0} messages have been moved.");
         }
     }
 }
