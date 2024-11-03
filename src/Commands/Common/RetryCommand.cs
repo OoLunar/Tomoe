7c06255031248dd77812e70d88647c2749dc47a4
Lunar Starstrum <lunar@forsaken-borders.net>
2024-11-02 20:58:09 -0500

    Add `retry` as a context menu command

--- src/Commands/Common/RetryCommand.cs
+++ src/Commands/Common/RetryCommand.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Commands.Processors.TextCommands;
 using DSharpPlus.Commands.Trees;
 using DSharpPlus.Commands.Trees.Metadata;
@@ -22,18 +23,18 @@ namespace OoLunar.Tomoe.Commands.Common
         /// </remarks>
         /// <param name="message">The message to re-run.</param>
         /// <param name="asWho">Who to run the command as.</param>
-        [Command("retry"), TextAlias("rerun")]
+        [Command("retry"), TextAlias("rerun"), SlashCommandTypes(DiscordApplicationCommandType.SlashCommand, DiscordApplicationCommandType.MessageContextMenu)]
         public static async ValueTask RetryAsync(CommandContext context, DiscordMessage message, DiscordUser? asWho = null)
         {
-            if (context is not TextCommandContext textCommandContext)
+            if (context is SlashCommandContext slashCommandContext)
             {
-                await context.RespondAsync("This command can only be used through text commands.");
-                return;
+                await slashCommandContext.RespondAsync("Sure! Let me try that again.", true);
             }
+
             // We use `is not true` since it could be null or false
-            else if (context.Client.CurrentApplication.Owners?.Contains(context.User) is not true
-                // If the message belongs to someone else or if we're changing the message to be sent as someone else
-                && (message.Author != context.User || (asWho is not null && asWho != context.User)))
+            if (context.Client.CurrentApplication.Owners?.Contains(context.User) is not true
+               // If the message belongs to someone else or if we're changing the message to be sent as someone else
+               && (message.Author != context.User || (asWho is not null && asWho != context.User)))
             {
                 await context.RespondAsync("You must be the bot owner to run this command as someone else.");
             }
@@ -42,7 +43,7 @@ namespace OoLunar.Tomoe.Commands.Common
                 await TextCommandUtilities.ModifyMessagePropertiesAsync(message, message.Content, context.Client, asWho, message.Channel!, message.Channel?.Guild);
             }
 
-            MessageCreatedEventArgs eventArgs = await TextCommandUtilities.CreateFakeMessageEventArgsAsync(textCommandContext, message, message.Content);
+            MessageCreatedEventArgs eventArgs = await TextCommandUtilities.CreateFakeMessageEventArgsAsync(context, message, message.Content);
             await context.Extension.GetProcessor<TextCommandProcessor>().ExecuteTextCommandAsync(context.Client, eventArgs);
         }
     }
