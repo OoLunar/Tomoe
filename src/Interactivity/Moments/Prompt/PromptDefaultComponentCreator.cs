2ed18450321e83fb66a93cac0da5541903a1b315
Lunar Starstrum <lunar@forsaken-borders.net>
2025-01-27 04:05:17 -0600

    Prompts should allow for paragraph responses

--- src/Interactivity/Moments/Prompt/PromptDefaultComponentCreator.cs
+++ src/Interactivity/Moments/Prompt/PromptDefaultComponentCreator.cs
@@ -6,7 +6,7 @@ namespace OoLunar.Tomoe.Interactivity.Moments.Prompt
     public class PromptDefaultComponentCreator : IPromptComponentCreator
     {
         public DiscordTextInputComponent CreateModalPromptButton(string question, Ulid id)
-            => new(question, id.ToString());
+            => new(question, id.ToString(), style: DiscordTextInputStyle.Paragraph);
 
         public DiscordButtonComponent CreateTextPromptButton(string question, Ulid id)
             => new(DiscordButtonStyle.Primary, id.ToString(), "Click here to answer", false);
